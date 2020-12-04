330c1499d7a810ea777f052df606ba13070b7d0e
trombone8417 <trombone8417@yahoo.com>
2020-12-04 12:59:26 +0800

    count total

--- admin/admin-dashboard.php
+++ admin/admin-dashboard.php
@@ -1,14 +1,159 @@
 <?php
 require_once 'assets/php/admin-header.php';
+require_once 'assets/php/admin-db.php';
+$count = new Admin();
 ?>
 <div class="row">
-    <h1>
-        <?= basename($_SERVER['PHP_SELF']) ?>
-    </h1>
-</div>
-<!-- footer -->
+    <div class="col-lg-12">
+        <div class="card-deck mt-3 text-light text-center font-weight-bold">
+            <div class="card bg-primary">
+                <div class="card-header">Total User</div>
+                <div class="card-body">
+                    <h1 class="display-4">
+                        <?= $count->totalCount('users'); ?>
+                    </h1>
+                </div>
+            </div>
+            <div class="card bg-warning">
+                <div class="card-header">Verfied User</div>
+                <div class="card-body">
+                    <h1 class="display-4">
+                        <?= $count->verified_users(1); ?>
+                    </h1>
+                </div>
+            </div>
+            <div class="card bg-success">
+                <div class="card-header">Unverified User</div>
+                <div class="card-body">
+                    <h1 class="display-4">
+                        <?= $count->verified_users(0); ?>
+                    </h1>
+                </div>
+            </div>
+            <div class="card bg-danger">
+                <div class="card-header">Website Hits</div>
+                <div class="card-body">
+                    <h1 class="display-4">
+                        120
+                    </h1>
+                </div>
+            </div>
         </div>
     </div>
-</div>   
+</div>
+<div class="row">
+    <div class="col-lg-12">
+        <div class="card-deck mt-3 text-light text-center font-weight-bold">
+            <div class="card bg-danger">
+                <div class="card-header">Total Notes</div>
+                <div class="card-body">
+                    <h1 class="display-4">
+                        <?= $count->totalCount('notes'); ?>
+                    </h1>
+                </div>
+            </div>
+            <div class="card bg-success">
+                <div class="card-header">Total Feedback</div>
+                <div class="card-body">
+                    <h1 class="display-4">
+                        <?= $count->totalCount('feedback'); ?>
+                    </h1>
+                </div>
+            </div>
+            <div class="card bg-info">
+                <div class="card-header">Total Notification</div>
+                <div class="card-body">
+                    <h1 class="display-4">
+                        <?= $count->totalCount('notification'); ?>
+                    </h1>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-lg-12">
+        <div class="card-deck my-3">
+            <div class="card border-success">
+                <div class="card-header bg-success text-center text-white lead">
+                    Male/Female User's Percentage
+                </div>
+                <div id="chartOne" style="width: 99%; height: 400px;"></div>
+            </div>
+            <div class="card border-info">
+                <div class="card-header bg-info text-center text-white lead">
+                    Verified/Unverified User's Percentage
+                </div>
+                <div id="chartTwo" style="width: 99%; height: 400px;"></div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- footer -->
+</div>
+</div>
+</div>
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+<script type="text/javascript">
+    // =============  男女比例圖表  ==================
+    google.charts.load('current', {
+        'packages': ['corechart']
+    });
+    google.charts.setOnLoadCallback(pieChart);
+
+    function pieChart() {
+        var data = new google.visualization.arrayToDataTable([
+            ['Gender', 'Number'],
+            <?php
+            $gender = $count->genderPer();
+            foreach ($gender as $row) {
+                echo '["' . $row['gender'] . '",' . $row['number'] . '],';
+            }
+
+            ?>
+
+        ]);
+
+        var options = {
+            is3D: false
+        };
+        var chart = new google.visualization.PieChart(document.getElementById('chartOne'));
+        chart.draw(data, options);
+    }
+    // ===========  驗證人數比例  =================
+    google.charts.load('current', {
+        'packages': ['corechart']
+    });
+    google.charts.setOnLoadCallback(colChart);
+
+    function colChart() {
+        var data = new google.visualization.arrayToDataTable([
+            ['Verified', 'Number'],
+            <?php
+            $verified = $count->verifiedPer();
+            foreach ($verified as $row) {
+                if ($row['verified'] == 0) {
+                    $row['verified'] = "Unverified";
+                } else {
+                    $row['verified'] = "Verified";
+                }
+                echo '["' . $row['verified'] . '",' . $row['number'] . '],';
+            }
+
+            ?>
+
+        ]);
+
+        var options = {
+            pieHole:0.4,
+        };
+        var chart = new google.visualization.PieChart(document.getElementById('chartTwo'));
+        chart.draw(data, options);
+    }
+</script>
 </body>
+
 </html>
--- admin/assets/php/admin-db.php
+++ admin/assets/php/admin-db.php
@@ -10,6 +10,43 @@
             $row = $stmt->fetch(PDO::FETCH_ASSOC);
             return $row;
         }
+        // 計算總數 
+        public function totalCount($tablename)
+        {
+            $sql = "SELECT * FROM $tablename";
+            $stmt = $this->conn->prepare($sql);
+            $stmt->execute();
+            $count = $stmt->rowCount();
+            return $count;
+        }
+        // 計算驗證使用者總數
+        public function verified_users($status)
+        {
+            $sql = "SELECT * FROM users WHERE verified = :status";
+            $stmt = $this->conn->prepare($sql);
+            $stmt->execute(['status'=>$status]);
+            $count = $stmt->rowCount();
+            return $count;
+        }
+        // 男女比例
+        public function genderPer()
+        {
+            $sql = "SELECT gender, COUNT(*) AS number FROM users WHERE gender !='' GROUP BY gender";
+            $stmt = $this->conn->prepare($sql);
+            $stmt->execute();
+            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            return $result;
+
+        }
+        // 23 32:05
+        public function verifiedPer()
+        {
+            $sql = "SELECT verified, COUNT(*) AS number FROM users GROUP BY verified";
+            $stmt = $this->conn->prepare($sql);
+            $stmt->execute();
+            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            return $result;
+        }
     }
 
 ?>
